66b950173052a46ce110b38c6f2f57f4ab8cc95b
KhailoAndrey <110947394+KhailoAndrey@users.noreply.github.com>
2023-01-31 23:09:28 +0000

    Deploying to gh-pages from @ KhailoAndrey/goit-js-hw-10@9a0dd458d0018cdb26fe2f7cdf8fa50e8ef69570 🚀

--- index.7d02858e.js
+++
@@ -1,2 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/users").then((function(t){if(!t.ok)throw new Error(t.status);return t.json()})).then((function(t){})).catch((function(t){}));
-//# sourceMappingURL=index.7d02858e.js.map
--- index.83a37130.js
+++
@@ -1,2 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/users").then((t=>{if(!t.ok)throw new Error(t.status);return t.json()})).then((t=>{})).catch((t=>{}));
-//# sourceMappingURL=index.83a37130.js.map
